2e575f16d320b75a920664681d266e0678523c48
JoshuaIrrazabal <ji8965@hotmail.com>
2023-06-23 18:36:49 -0400

    search function is implemented

--- lab_assignment_6.c
+++ lab_assignment_6.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int search(int numbers[], int low, int high, int value) 
-{
-	return -1;
+/*Write an efficient recursive function that takes in a sorted array of numbers, two integers, low and high,
+representing indexes into the array, and another integer, value, and returns the index in the array where
+value is found in the array in between index low and high, inclusive. If value is NOT found in the array in
+between indexes low and high, inclusive, then the function should return -1*/
+
+int search(int numbers[], int low, int high, int value) {
+	if (low > high)
+		return -1; //incase someone thinks 1 is more than 2.
+	int mid = (low+high)/2; //for the value between low and high
+
+	if(value<numbers[mid])//case value is lower than mid
+		return search(numbers, low, mid-1, value);//mid become new high, mid is recalculated, search is called again
+	else if(value>numbers[mid])//case value is higher than mid
+		return search(numbers, mid+1, high, value);//mid becomes new low, mide is recalculated, search is called again
+	else
+		return mid;
 }
 
 void printArray(int numbers[], int sz)
